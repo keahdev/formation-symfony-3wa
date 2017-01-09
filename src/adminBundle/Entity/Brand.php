02718961ffb97febd5d480948d3b3bce3a23705b
Your Name <ke.ahcene@gmail.com>
2017-01-09 17:51:50 +0100

    Formation Symfony

---
+++ src/adminBundle/Entity/Brand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace adminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Brand
+ *
+ * @ORM\Table(name="brand")
+ * @ORM\Entity(repositoryClass="adminBundle\Repository\BrandRepository")
+ */
+class Brand
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Titre", type="string", length=100)
+     */
+    private $titre;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Brand
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+}
+
